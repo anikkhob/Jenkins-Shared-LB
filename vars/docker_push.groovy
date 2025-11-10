09d1435c65879c5a15ff578e51eea74075d4e60a
Aniket Khobragade <118123205+anikkhob@users.noreply.github.com>
2025-11-10 19:20:44 +0530

    Update Docker credentials variable name

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'Docker_hub_Access', usernameVariable: 'dockerhubuser')]) {
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
