23d525de5564b5c8f002f7e78118311a5aec11c5
Aniket Khobragade <118123205+anikkhob@users.noreply.github.com>
2025-11-12 18:43:11 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ImageTag, String DockerHubUser){
+def call(String ProjectName,String ImageTag, String DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
