a45a10e6a353a66989ebfe5027a960ad3275d704
Aniket Khobragade <118123205+anikkhob@users.noreply.github.com>
2025-11-25 21:24:08 +0530

    Fix syntax and method name in SonarQube quality check

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,6 @@
-def call(){
-  timeout(time:1  Unit:"MINUTES"){
-    waitforQualityGate abortPipeline: false
+
+    def call(){
+  timeout(time: 1, unit: "MINUTES"){
+      waitForQualityGate abortPipeline: false
   }
-  {
+}
